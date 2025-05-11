ddfe788ef9420dec44ab61c760ceabc1c28b7559
nedbajak <nedbajak@fel.cvut.cz>
2025-05-11 21:13:50 +0200

    zkousim vse

--- FrontEnd/src/pages/ReportForm.jsx
+++ FrontEnd/src/pages/ReportForm.jsx
@@ -42,6 +42,8 @@ function ReportForm() {
     const [formError, setFormError] = useState("");
 
     useEffect(() => {
+        console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+
         if (user) {
             setForm((prev) => ({
                 ...prev,
